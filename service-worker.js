9c7c39e2d55f6b204dc7f1002064b9d1f1c2fa40
y68dnx <yousifabdulsattar1q@gmail.com>
2026-06-02 10:29:15 +0300

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'hatchery-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
